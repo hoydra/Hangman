594164cd37ca9fdba5ab8951082c63dc25796866
Roberto Cellini <rcellini13@gmail.com>
2021-09-16 20:03:58 +0200

    fix rand_word

--- main.py
+++ main.py
@@ -96,62 +96,83 @@ def rand_word():
 
 
 class Hangman:
-    SECRETWORD = random.choice(rand_word())
-    print(SECRETWORD)
-    correct = list("_" * len(SECRETWORD))
+
+    secretword = random.choice(rand_word())
+    correct = list("_" * len(secretword))
     wrong = []
+    guessed = []
     life = 6
+    status = ""
     image = 0
 
 
     @staticmethod
     def interface():
-        print(hangmanpics[Hangman.image])
-        print("".join(Hangman.correct))
+
+        try:
+            print(hangmanpics[Hangman.image])
+            print("".join(Hangman.correct))
+            print(f"wrong: {Hangman.wrong}")
+            print(Hangman.status)
+        except Exception:
+            print(f"You lost, the word was {Hangman.secretword}")
+            return True
 
     @staticmethod
     def player():
+        Hangman.status = ""
 
-        if "".join(Hangman.correct) == Hangman.SECRETWORD:
+        if Hangman.life > 0 or Hangman.image > 4:
+            Hangman.status = "lost"
+
+        if "".join(Hangman.correct) == Hangman.secretword:
             return True
 
         guess = input(" : ")
-        if len(guess) != 1:
-            print("one letter only ! -1 life")
 
-        if guess.isnumeric():
-            print("no numbers ! -1 life")
-
-        if guess not in Hangman.SECRETWORD or guess in Hangman.wrong:
-            print(f"{guess} is not in the word !")
+        if guess not in Hangman.secretword or guess in Hangman.wrong:
+            Hangman.status = f"{guess} is not in the word !"
             Hangman.wrong.append(guess)
             Hangman.life -= 1
             Hangman.image += 1
 
-        # make a function to not accept duplicates, so if we write m and then m again one life -= 1
+        if len(guess) != 1:
+            Hangman.status = "one letter only ! -1 life"
 
+        if guess.isnumeric():
+            Hangman.status = "no numbers ! -1 life"
 
+        if guess in Hangman.guessed:
+            Hangman.status = f"{guess} already guessed"
 
-        for count,i in enumerate(list(Hangman.SECRETWORD)):
+        for count, i in enumerate(list(Hangman.secretword)):
 
             if guess == i:
+                Hangman.guessed.append(guess)
                 Hangman.correct[count] = guess
 
     def game(self):
         end = False
         won = False
+
         while end == False:
             self.interface()
             if self.player():
                 won = True
+                end = True
+            if self.interface():
+                end = True
             if won:
-                for i in range(5):
+
+                for i in range(2):
                     print(victory_figure[0])
+                    print("You")
                     time.sleep(1)
                     print(victory_figure[1])
+                    print("Won!")
                     time.sleep(1)
-
                     end = True
+
 
 
 if __name__ == '__main__':
